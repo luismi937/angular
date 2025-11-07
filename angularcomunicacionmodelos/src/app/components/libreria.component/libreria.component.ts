1e6f94a48f69fef820be3d8b9457404c2741747c
luismi937 <luismicanizares02@gmail.com>
2025-11-07 10:13:08 +0100

    up

--- angularcomunicacionmodelos/src/app/components/libreria.component/libreria.component.ts
+++ angularcomunicacionmodelos/src/app/components/libreria.component/libreria.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Comic } from '../../models/producto/comics';
+
 
 @Component({
   selector: 'app-libreria',
@@ -7,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrl: './libreria.component.css',
 })
 export class LibreriaComponent {
+@ViewChild("cajanombre") cajaNombre !: ElementRef;
+@ViewChild("cajadescripcion") cajaDescripcion !: ElementRef;
+@ViewChild("cajaimagen") cajaimagen!: ElementRef;
+  public comics: Array<Comic>; 
+  public comicFavorito!: Comic;
 
+  createComic(): void{
+    let nombre = this.cajaNombre.nativeElement.value
+    let descripcion = this.cajaDescripcion.nativeElement.value
+    let imagen = this.cajaimagen.nativeElement.value
+    let comicNew = new Comic(nombre, imagen, descripcion);
+    this.comics.push(comicNew)
+  }
+  seleccionarFavorito(favorito: Comic): void{
+    this.comicFavorito = favorito;  
+  }
+  deleteComic(index: number){
+    this.comics.splice(index, 1)
+  }
+  constructor(){
+    this.comics = [
+      new Comic(
+        "Spiderman",
+        "https://images-na.ssl-images-amazon.com/images/I/61AYfL5069L.jpg",
+        "Hombre araña"
+      ),
+      new Comic(
+        "Wolverine",
+        "https://i.etsystatic.com/9340224/r/il/42f0e1/1667448004/il_570xN.1667448004_sqy0.jpg",
+        "Lobezno"
+      ),
+      new Comic(
+        "Guardianes de la Galaxia",
+        "https://cdn.normacomics.com/media/catalog/product/cache/1/thumbnail/9df78eab33525d08d6e5fb8d27136e95/g/u/guardianes_galaxia_guadianes_infinito.jpg",
+        "Yo soy Groot"
+      ),
+      new Comic(
+      "Avengers",
+      "https://d26lpennugtm8s.cloudfront.net/stores/057/977/products/ma_avengers_01_01-891178138c020318f315132687055371-640-0.jpg",
+      "Los Vengadores"
+      ),
+      new Comic(
+      "Spawn",
+      "https://i.pinimg.com/originals/e1/d8/ff/e1d8ff4aeab5e567798635008fe98ee1.png",
+      "Todd MacFarlane"
+      )
+    ]; 
+  }
 }
