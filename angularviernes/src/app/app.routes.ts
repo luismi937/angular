cfbf6f1c778fd701c1f29fb2b920ec456ab37839
luismi937 <luismicanizares02@gmail.com>
2025-11-07 14:17:39 +0100

    up

--- angularviernes/src/app/app.routes.ts
+++ angularviernes/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home.component';
-import{PersonasComponent} from '../../src/app/components/personas.component';
+import { PersonasComponent } from './../app/components/personas.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
